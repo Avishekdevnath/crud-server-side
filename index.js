e65d0f79098e323adfb4a72f4efd213fc8d06932
SujoyDevnath <sujoydevnathdipta@gmail.com>
2021-12-11 22:00:32 +0600

    s

--- index.js
+++ index.js
@@ -40,6 +40,15 @@ async function run() {
             console.log(user);
             res.json(result);
         });
+
+        // DELETING USERS DATA
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.json(result);
+        })
+
         // // CHECKING ADMIN
         // app.get('/users/:email', async (req, res) => {
         //     const email = req.params.email;
